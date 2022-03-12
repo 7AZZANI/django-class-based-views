da1858e415972d61bd68ca13ca1358af7b215153
king9 <agz97agz97@gmail.com>
2022-03-12 13:49:44 +0800

    add static & media fiels config into settings

--- post/models.py
+++ post/models.py
@@ -3,11 +3,13 @@ from unicodedata import name
 from django.db import models
 from django.forms import CharField
 from pytz import timezone
+from django.utils import timezone
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=1000)
-    created_at = models.DateTimeField(default=timezone)
+    created_at = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to='post-img/')
 
     
@@ -15,9 +17,9 @@ class Post(models.Model):
     class Meta:
         verbose_name = ("post")
         verbose_name_plural = ("posts")
-
+# الي تحت هذا لتجعل اسم المنشور الذي عملته يظهل بدل كلمة اوبجيكت , التايتل تتغير على حسن الفونكشون
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
